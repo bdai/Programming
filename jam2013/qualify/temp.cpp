4ae5f18cc8066d2f2c23e22d08b5d9bf99c09c9f
Bin Dai <daibin5@gmail.com>
2013-04-13 14:07:25 -0400

    jam2013 tic-toc-toe

---
+++ jam2013/qualify/temp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; t++) {
+      cout << "Case #" << t << ": ";
+      cout << endl;
+    }
+    return 0;    
+}
